862056dbc4acd718545c480ff6dff4aba3188e06
Ramyahc2002 <ramyaramyahc71@gmail.com>
2024-05-15 15:43:12 -0600

    hi

---
+++ script.js
@@ -0,0 +1,15 @@
+function validateage(){
+    var name = document.getElementById("name")
+    var age =  document.getElementById("age").value;
+
+    if(age>=18)
+        {
+        alert("You are eligable to vote");
+}
+else
+{
+    alert("you are still child😂");
+
+}
+
+}
